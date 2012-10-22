e08dc1daeea194365dac8297c3606681ecd9616e
astone-solutions <ldreuillat@gmail.com>
2012-10-22 08:14:32 +0200

    Add LGPL comment in files header

--- PluginVSTO/OutlookAddin_2003/OutlookAddIn1/OutlookAddIn1/Installer1.cs
+++ PluginVSTO/OutlookAddin_2003/OutlookAddIn1/OutlookAddIn1/Installer1.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+* (C) Copyright 2012 Astone Solutions (http://astone-solutions.fr/) and contributors.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser General Public License
+* (LGPL) version 2.1 which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl.html
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
